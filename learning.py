1b56d7a7c2d853e4eb43e37507664e874855ea0c
Arkajyoti Paul <arkajyotislg12@gmail.com>
2024-01-05 02:06:22 +0530

    Modified

--- learning.py
+++ learning.py
@@ -9,3 +9,11 @@ print('The value of x after swapping: {}'.format(x))
 print('The value of y after swapping: {}'.format(y))
 
 print ("Hello World")
+
+def sum(n):
+    add = 0
+    for i in range (1,n+1):
+        add +=i*i*i
+    return add
+n=5
+print (sum(n))
